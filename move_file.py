f5f6fc5d4e03c729f4414ad506ff404ad7eb893b
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2023-06-26 13:29:53 +0300

    Update move_file.py

--- move_file.py
+++ move_file.py
@@ -1,22 +1,23 @@
 import os
 import shutil
-from_dir="/Users/shauryakumar/Documents/Document_Files"
-to_dir="/Users/shauryakumar/Desktop/Project102"
+
+from_dir="/Users/shauryakumar/Downloads"
+to_dir="/Users/shauryakumar/Desktop/Document_files"
+
 list_of_files=os.listdir(from_dir)
 print(list_of_files)
 for file_name in list_of_files:
     name,ext=os.path.splitext(file_name)
-    print(name,ext)
     if ext==" ":
         continue
-    if ext in [".txt", ".doc", ".docx", ".pdf",".png"]:
-       path1=from_dir+"/"+file_name
-       path2=to_dir+"/"+"Document_files"
-       path3=to_dir+"/"+"Document_files"+"/"+file_name
-    if os.path.exists(path2):
-       print("moving"+file_name+"....")
-       shutil.move(path1,path3)
-    else:
-       os.makedirs(path2)
-       print("moving"+file_name+"....")
-       shutil.move(path1,path3)
+    if ext in [".txt",".doc",".docx",".pdf"]:
+        path1=from_dir+"/"+file_name
+        path2=to_dir+"/"+"image_files"
+        path3=to_dir+"/"+"image_files"+"/"+file_name
+        if os.path.exists(path2):
+            print("moving"+file_name+"....")
+            shutil.move(path1,path3)
+        else:
+            os.makedirs(path2)
+            print("moving"+file_name+"....")
+            shutil.move(path1,path3)
